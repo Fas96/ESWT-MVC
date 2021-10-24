45a18ade8b895487f4c722172171645ea442cb15
Fas96 <firibu.anyass@gmail.com>
2021-10-25 00:16:05 +0900

    form files uploaded to directory

--- src/main/java/com/example/controller/QuestionController.java
+++ src/main/java/com/example/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.dao.QuestionMapper;
 import com.example.entity.Employee;
 import com.example.entity.Question;
+import com.example.util.FileUploadUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.Objects;
 import java.util.logging.Logger;
 
@@ -25,7 +27,7 @@ public class QuestionController {
 
 
     @PostMapping(value = "/save" )
-    public String saveEmployee(Question question , @RequestParam("image1") MultipartFile question_media,@RequestParam("image2") MultipartFile question_second){
+    public String saveEmployee(Question question , @RequestParam("image1") MultipartFile question_media,@RequestParam("image2") MultipartFile question_second) throws IOException {
 
 
          if(!question_media.isEmpty() || !question_second.isEmpty() ){
@@ -34,10 +36,18 @@ public class QuestionController {
               String question_second_name= StringUtils.cleanPath(Objects.requireNonNull(question_second.getOriginalFilename()));
               question.setQuestion_media(question_media_name);
               question.setQuestion_second(question_second_name);
+             String uploadDir="question-images/"+question.getQuestion_title();
+             //clean-> delete old files in directory before saving file to directory
+             //FileUploadUtil.clearDir(uploadDir);
+//             String uploadDir, String fileName, MultipartFile multipartFile
+             if(!question_second.isEmpty()){
+
+                 System.out.println("===============question_media================");
+                 FileUploadUtil.saveFile(uploadDir,question_media_name,question_media);
+             } if(!question_media.isEmpty()){
+                 System.out.println("===============question_second================");
+             FileUploadUtil.saveFile(uploadDir,question_second_name,question_second);}
           }
-
-
-
         if(question.getId() == null){
             questionMapper.saveQuestion(question);
         }else{
